64367fd2acf7285c42cbe72256fccce6fba6c967
duzhg56980 <guokey1996@gmail.com>
2024-02-19 14:28:48 +0800

    init commit

---
+++ 397020-4.5.16/FunctionUse382.java
@@ -0,0 +1,6 @@
+import cn.hutool.http.webservice.SoapClient;
+public class FunctionUse382 {
+public void funcUse() {
+SoapClient soapclient = new SoapClient();
+}
+}
---
+++ 397020-4.5.16/FunctionUse395.java
@@ -0,0 +1,7 @@
+import cn.hutool.http.webservice.SoapUtil;
+public class FunctionUse395 {
+public void funcUse() {
+SoapUtil soaputil = new SoapUtil();
+soaputil.toString(null,null);
+}
+}
